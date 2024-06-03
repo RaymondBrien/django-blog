b0a0ee675a375a1d79c8ba4ebaf0fe42c6837553
Raymond Brien <raytbrien@gmail.com>
2024-06-03 15:43:57 +0000

    Update Comment model to revert testing fields

--- blog/models.py
+++ blog/models.py
@@ -14,6 +14,7 @@ class Post(models.Model):
     status = models.IntegerField(choices=STATUS, default=0) # default saves as draft
     excerpt = models.TextField(blank=True)
     updated_on = models.DateTimeField(auto_now=True)
+    
     class Meta: 
         ordering = ["-created_on"]
     def __str__(self):
@@ -25,5 +26,10 @@ class Comment(models.Model):
     body = models.TextField()
     approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        ordering = ["created_on"]
+    def __str__(self):
+        return f"{self.body} by {self.author}"
     
     
