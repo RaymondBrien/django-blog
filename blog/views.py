d0ee6d4dd1f0972c3b0995a48865afdd30aeb9b6
Raymond Brien <raytbrien@gmail.com>
2024-06-04 11:38:08 +0000

    Add PostList class view

--- blog/views.py
+++ blog/views.py
@@ -4,5 +4,9 @@ from .models import Post
 
 # Create your views here.
 class PostList(generic.ListView):
-    queryset = Post.objects.all()
-    template_name = "post_list.html"
+    queryset = Post.objects.filter(status=1)
+    # automatically expects a template called post_list 
+    # given the Model name and the type of view: here as Post and Listview. 
+    # Should not change file name in workspace but can use template_name
+    # to give it a custom name if we want 
+    template_name = "post_list.html" 
