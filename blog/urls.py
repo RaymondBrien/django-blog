0f3ee3faada9bdb458d749f3c388961bac491e3d
Raymond Brien <raytbrien@gmail.com>
2024-06-07 15:36:43 +0000

    Add post_detail view

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,6 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.PostList.as_view(), name='home') # name can be referenced in template logic statements
+    path('', views.PostList.as_view(), name='home'), # name referenced in template logic statements
+    path('<slug:slug>/', views.post_detail, name='post_detail'),
 ]
