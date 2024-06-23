44e5f9da257466b346a08e8a703cf2ae1a6d4b7e
Raymond Brien <raytbrien@gmail.com>
2024-06-23 09:29:19 +0000

    Add collaboration form functionality to about page

--- about/views.py
+++ about/views.py
@@ -1,15 +1,51 @@
 from django.shortcuts import render
 from django.views import generic
-from .models import About
+from .models import About, CollaborateRequest
+from django.contrib import messages
+from .forms import CollaborateForm
 
 def about_me(request):
     """
     Renders the about page
     """
     about = About.objects.all().order_by('-updated_on').first()
+
+    if request.method == "POST":
+        print('Reveived POST request')
+        collaborate_form = CollaborateForm(data=request.POST)
+        if collaborate_form.is_valid():
+            collaborate_form.save()
+            messages.add_message(
+                request,
+                messages.SUCCESS,
+                'Message sent successfully, I aim to respond in two working days.'
+            )
+    collaborate_form = CollaborateForm()
     
-    return render(
+    return render ( # send variables to be accesssible in template
         request,
         "about/about.html",
-        {"about": about} # enables us to use 'about' variable in actual template, as it's defined here
+        {"about": about,
+        "collaborate_form": collaborate_form,
+        }
     )
+
+#     
+#     Your views.py file should now look like this:
+# from django.shortcuts import render
+# from .models import About
+# from .forms import CollaborateForm
+
+
+# def about_me(request):
+#     about = About.objects.all().order_by('-updated_on').first()
+#     collaborate_form = CollaborateForm()
+
+#     return render(
+#         request,
+#         "about/about.html",
+#         {
+#             "about": about,
+#             "collaborate_form": collaborate_form
+#         },
+#     )
