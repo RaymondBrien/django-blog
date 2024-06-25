ba455d221ee45b918d6607c928f8e031d0822211
Raymond Brien <raytbrien@gmail.com>
2024-06-25 16:01:59 +0000

    Add template logic for image display

--- codestar/settings.py
+++ codestar/settings.py
@@ -29,7 +29,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-raymondbrien-djangoblog-6qn1h5a302d.ws-eu114.gitpod.io', '8000-raymondbrien-djangoblog-2f7r6pchwcs.ws.codeinstitute-ide.net', '.herokuapp.com']
 
